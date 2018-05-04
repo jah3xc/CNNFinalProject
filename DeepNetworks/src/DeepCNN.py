160efa409af3de73e535d932233b937bd7755496
James Hurt <jah3xc@mail.missouri.edu>
2018-05-04 11:59:24 -0500

    Change from 4 to 3

--- DeepNetworks/src/DeepCNN.py
+++ DeepNetworks/src/DeepCNN.py
@@ -17,6 +17,9 @@ class DeepCNN:
         self.num_classes = 0
         self.num_epochs = num_epochs
         self.num_folds = num_folds
+        self.cross_validation_accuracy = {}
+        self.average_accuracy = 0.
+        self.model = None
 
         # get the folds
         self.folds = generate_folds(self.dirname, self.num_folds)
@@ -61,4 +64,64 @@ class DeepCNN:
         """
         Train the network
         """
-        pass
+        # for each fold
+        for fold in [chr(i) for i in range(65, 65 + self.num_folds)]:
+
+            logging.info("Training Fold {}".format(fold))
+            # create fresh model
+            del self.model
+            self.compile()
+            # genearate filelist for training this fold
+            training_files = []
+            for f in self.folds:
+                if f != fold:
+                    training_files.append(self.folds[f])
+            # flatten the array
+            training_files = [
+                item for sublist in training_files for item in sublist
+            ]
+            # generate the training data
+            data, classes = generate_train_data(training_files, self.img_size)
+            # sanity check
+            if len(data) != len(classes):
+                logging.critical(
+                    "Training Data and Labels have different lengths ({} vs {})!".
+                    format(len(data), len(classes)))
+                return
+
+            logging.debug("Found {} training images!".format(len(data)))
+
+            # fit the model
+            try:
+                self.model.fit(data, classes, epochs=self.num_epochs)
+            except Exception as err:
+                logging.critical("Could not train! An error occurred")
+                logging.debug(err)
+                return
+
+            # test the model
+            test_data, test_classes = generate_train_data(
+                self.folds[fold], self.img_size)
+            # sanity check
+            if len(test_data) != len(test_classes):
+                logging.critical(
+                    "Testing Data and Labels have different lengths ({} vs {})!".
+                    format(len(test_data), len(test_classes)))
+                return
+            # eval
+            try:
+                loss, accuracy = self.model.evaluate(test_data, test_classes)
+                self.cross_validation_accuracy[fold] = accuracy
+            except Exception as err:
+                logging.critical("Could not test! An error occurred")
+                logging.debug(err)
+                return
+
+            print("Fold {} Accuracy: {}".format(
+                fold, self.cross_validation_accuracy[fold]))
+
+        total = 0.
+        for score in self.cross_validation_accuracy:
+            total += float(self.cross_validation_accuracy[score])
+        self.average_accuracy = total / self.num_folds
+        print("Average Accuracy: {}".format(self.average_accuracy))
